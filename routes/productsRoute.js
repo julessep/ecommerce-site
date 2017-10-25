ab31fec23e6d117ca9e12f8a9ba6f09c9d332fc1
Jules Sepulveda <julietrsepulveda@gmail.com>
2017-10-24 21:14:59 -0500

    added view single product

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -6,12 +6,14 @@ const router = Router();
 const {
   getProducts,
   postProduct,
-  addProductForm
+  addProductForm,
+  getOneProduct
 } = require('../controllers/productCtrl.js');
 
 // need to change to only get top 20 
 router.get('/bestsellers', getProducts);
 router.get('/products', getProducts);
+router.get('/products/:id', getOneProduct);
 router.get('/postProduct', addProductForm);
 router.post('/postProduct', postProduct);
 
